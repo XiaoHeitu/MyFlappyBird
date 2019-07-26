2ad8039741663973c199345aa8db2872148b57a2
黑色幽魂 <ChineseYwj@hotmail.com>
2019-07-26 20:01:56 +0800

    建立版本库

---
+++ assets/MainScript.ts
@@ -0,0 +1,38 @@
+// Learn TypeScript:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/typescript.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class MainScript extends cc.Component {
+
+    @property(cc.Sprite)
+    bird: cc.Sprite = null;
+
+    // @property
+    // text: string = 'hello';
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad () {
+        cc.director.getPhysicsManager().enabled = true;
+        this.node.on('touchstart', this.onTouchStart,this);
+    }
+
+    start () {
+
+    }
+
+    // update (dt) {}
+    onTouchStart(event:cc.Event.EventTouch){
+        var body=this.bird.getComponent(cc.RigidBody);
+        body.applyLinearImpulse(cc.v2(0,50), body.getWorldCenter(),true);
+    }
+}
---
+++ assets/PipeScript.ts
@@ -0,0 +1,43 @@
+// Learn TypeScript:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/typescript.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class NewClass extends cc.Component {
+
+    // @property(cc.Label)
+    // label: cc.Label = null;
+
+    // @property
+    // text: string = 'hello';
+
+    // @property(cc.Sprite)
+    // bg:cc.Sprite=null;
+    
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {}
+
+    start () {
+
+    }
+
+    update (dt) {
+        var offest=(dt/1)*50;
+        this.node.width=this.node.width + offest;
+        var widget=this.getComponent(cc.Widget);
+        widget.left=widget.left-offest;
+
+        console.log(widget.left);
+
+    }
+}
